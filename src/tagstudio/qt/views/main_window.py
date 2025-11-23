498dd1a94e79d501fc89ff33279746df9d9a837a
TrigamDev <trigambusiness@gmail.com>
2025-11-23 15:47:24 -0500

    allat

--- src/tagstudio/qt/views/main_window.py
+++ src/tagstudio/qt/views/main_window.py
@@ -36,9 +36,8 @@ from PySide6.QtWidgets import (
 )
 
 from tagstudio.core.enums import ShowFilepathOption
-from tagstudio.core.library.alchemy.enums import SortingModeEnum
-from tagstudio.qt.controllers.preview_panel.preview_panel_controller import PreviewPanel
 from tagstudio.core.library.alchemy.enums import SortingModeEnum, TagColorEnum
+from tagstudio.qt.controllers.preview_panel.preview_panel_controller import PreviewPanel
 from tagstudio.qt.helpers.color_overlay import theme_fg_overlay
 from tagstudio.qt.mixed.landing import LandingWidget
 from tagstudio.qt.mixed.pagination import Pagination
